3c82260f57254ecdc686078f273e528103c7611a
ofkTuhin <tuhin.omar99@gmail.com>
2022-10-30 21:54:05 +0600

    this is python

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
 print("hello")
+  #variable in python
+  #string type/ carecter type
+num="tuhin" 
+print(num)
+#number
+#integer
+num1=2
+#floating
+num2=2.1
+print(type(num1*num2))
+
+#strint slice
+string="This is my first python code"
+print(string[:4] + string[2:9])
 
