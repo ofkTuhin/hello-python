0e8a6ddcbe9cf119d47c0058799a9c830461b748
ofkTuhin <tuhin.omar99@gmail.com>
2022-11-04 12:14:48 +0600

    solving even and number problem

---
+++ imprt.py
@@ -0,0 +1,16 @@
+# for loop in python
+
+number=int(input("enter a number:"))
+
+
+
+
+
+
+if len(str(number))<=100:
+   if number%2==0:
+       print("even")
+   else:
+       print("odd")
+else:
+    print("this number is large")
